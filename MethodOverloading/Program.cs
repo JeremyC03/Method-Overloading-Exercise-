ef876110cb7ce7b0b4368fd9a0dd9ef3f2e81e2f
Jeremy Cabrera <j3r3myc03@yahoo.com>
2023-06-26 15:10:00 -0400

    Finished Exercise 1

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace MethodOverloading
+﻿using System;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(10, 9));
+            Console.WriteLine(Add(3.14m, 1.17m));
+            Console.WriteLine(Add(13, 21, true));
+
+        }
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+        public static string Add(int x, int y, bool isTrue)
+        {
+            int sum = x + y;
+
+            if (isTrue == true && sum == 1)
+            {
+                return $"{sum} dollar.";
+            }
+            else if (isTrue == true && sum != 1)
+            {
+                return $"{sum} dollars.";
+            }
+            else
+            {
+                return sum.ToString();
+            }
         }
     }
 }
